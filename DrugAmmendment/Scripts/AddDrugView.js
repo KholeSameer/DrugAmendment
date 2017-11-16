2324a9491d2f085ea3a65cfae23073b74d025d6b
SameerKhole <kholesameer110@gmail.com>
2017-11-16 11:56:21 +0530

    Separating JS Code

---
+++ DrugAmmendment/Scripts/AddDrugView.js
@@ -0,0 +1,31 @@
+﻿
+            $(function () {
+                $("#criteria").autocomplete({
+                    source: function (request, response) {
+                        var param = { criteria: $('#criteria').val(), delivery: $('#client').val(), criteriaType: $('#criteriaType').val() };
+                        $.ajax({
+                            url: "GetAutoTHSTerm",
+                            data: JSON.stringify(param),
+                            dataType: "json",
+                            type: "POST",
+                            contentType: "application/json; charset=utf-8",
+                            dataFilter: function (data) { return data; },
+                            success: function (data) {
+                                console.log(data);
+                                response($.map(data, function (item) {
+                                    return {
+                                        label: item,
+                                        value: item
+                                    };
+                                }));
+
+                            },
+                            error: function (err) {
+                                debugger;
+                                alert(err + "   Some Error Message");
+                            }
+                        });
+                    },
+                    minLength: 3
+                });
+            });
