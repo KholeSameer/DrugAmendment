412c412c64d1e83febb97992d3507fd8cffcf395
SameerKhole <kholesameer110@gmail.com>
2017-11-20 11:40:07 +0530

    Export To Excel Successful

---
+++ DrugAmmendment/Models/ExportToExcel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrugAmmendment.Models
+{
+    public class ExportToExcel
+    {
+        public string Delivery { get; set; }
+        public string CriteriaType { get; set; }
+        public string Criteria { get; set; }
+        public int? TermID { get; set; }
+        public string ModificationDate { get; set; }
+        public string CreationDate { get; set; }
+    }
+}
