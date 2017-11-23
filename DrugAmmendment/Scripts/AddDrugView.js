287ec8b2b7ac0c55ae1fdd071226b78b16e34fed
SameerKhole <kholesameer110@gmail.com>
2017-11-23 15:18:53 +0530

    After 1st Demo

--- DrugAmmendment/Scripts/AddDrugView.js
+++ DrugAmmendment/Scripts/AddDrugView.js
@@ -1,31 +1,39 @@
 ﻿
-            $(function () {
-                $("#criteria").autocomplete({
-                    source: function (request, response) {
-                        var param = { criteria: $('#criteria').val(), delivery: $('#client').val(), criteriaType: $('#criteriaType').val() };
-                        $.ajax({
-                            url: "GetAutoTHSTerm",
-                            data: JSON.stringify(param),
-                            dataType: "json",
-                            type: "POST",
-                            contentType: "application/json; charset=utf-8",
-                            dataFilter: function (data) { return data; },
-                            success: function (data) {
-                                console.log(data);
-                                response($.map(data, function (item) {
-                                    return {
-                                        label: item,
-                                        value: item
-                                    };
-                                }));
+$(document).ready(function () {
+    var isBrandname = $('#criteriaType').val();
+    if (isBrandname == 'BrandName') {
+    }
+    else {
+        $(function () {
+            $("#criteria").autocomplete({
+                source: function (request, response) {
+                    var param = { criteria: $('#criteria').val(), delivery: $('#client').val(), criteriaType: $('#criteriaType').val() };
+                    $.ajax({
+                        url: "GetAutoTHSTerm",
+                        data: JSON.stringify(param),
+                        dataType: "json",
+                        type: "POST",
+                        contentType: "application/json; charset=utf-8",
+                        dataFilter: function (data) { return data; },
+                        success: function (data) {
+                            console.log(data);
+                            response($.map(data, function (item) {
+                                return {
+                                    label: item,
+                                    value: item
+                                };
+                            }));
 
-                            },
-                            error: function (err) {
-                                debugger;
-                                alert(err + "   Some Error Message");
-                            }
-                        });
-                    },
-                    minLength: 3
-                });
+                        },
+                        error: function (err) {
+                            debugger;
+                            alert(err + "   Some Error Message");
+                        }
+                    });
+                },
+                minLength: 3
             });
+        });
+    }
+})
+            
